f7b00f16e8f08312b18ab82db259af6f1f1f93f1
harshalsabale <30774666+harshalsabale@users.noreply.github.com>
2024-04-16 22:25:32 +0530

    Adding Token Bucket Based Rate limiter

---
+++ src/main/java/org/ratelimiter/RateLimiter.java
@@ -0,0 +1,9 @@
+package org.ratelimiter;
+
+
+
+public interface RateLimiter {
+
+    boolean isRequestAllowed();
+
+}
--- src/main/java/org/ratelimiter/RateLimiterConfiguration.java
+++ src/main/java/org/ratelimiter/RateLimiterConfiguration.java
@@ -5,8 +5,14 @@ import lombok.Getter;
 @Getter
 public class RateLimiterConfiguration {
 
+    RateLimiter rateLimiter;
     private int capacity = 0;
     private double fillRate = 0;
+
+    public RateLimiterConfiguration() {
+        this.rateLimiter = new TokenBucketBasedRateLimiter(capacity, fillRate);
+    }
+
     public RateLimiterConfiguration capacity(int capacity) {
         this.capacity = capacity;
         return this;
@@ -17,4 +23,7 @@ public class RateLimiterConfiguration {
         return this;
     }
 
+    public RateLimiter configure() {
+        return new TokenBucketBasedRateLimiter(capacity, fillRate);
+    }
 }
